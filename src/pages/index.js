bc1a84ecbc44a38f4529060e2974103363f40153
Esseson <Essesonahmed@yahoo.com>
2019-03-12 05:13:43 -0700

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to my new portfolio website.</p>
+    <p>feel free to contact us for all your designs.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
